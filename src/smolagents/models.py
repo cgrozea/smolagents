cfe555615d66654c86260feef3757f4e599205df
C.G. <cgrozea@users.noreply.github.com>
2025-06-28 15:06:40 +0200

    Update models.py

--- src/smolagents/models.py
+++ src/smolagents/models.py
@@ -1134,7 +1134,7 @@ class LiteLLMModel(ApiModel):
         ax=response.choices[0].message.model_dump(include={"role", "content", "tool_calls"})
         S=ax['content']
         try:
-            S = S[:S.find("<think>")] + S[S.rfind("</think>") + 5:]
+            S = S[:S.find("<think>")] + S[S.rfind("</think>") + 8:]
             ax['content'] = S
         except:
             pass
